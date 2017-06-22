3e6fa919c3ed73bd6d8895f017ce428cc26d8d1b
AndersonVivas <vivas251@gmail.com>
2017-06-21 22:15:00 -0500

    Actualice index.bandle.php
    agregue el botón eliminar ya esta funcional

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -51,11 +51,9 @@
                                     
                                     <td>
                                         <a class="btn btn-info" href="{{URL::action('ClienteController@edit',$c->IDCLIENTE)}}">Editar</a>
-                                    </td>
-                                    <td>
                                         <a class="btn btn-info" href="{{URL::action('ClienteController@destroy',$c->IDCLIENTE)}}">Eliminar</a>
                                     </td>
-                                </tr>
+                                   </tr>
                                 @endforeach
                             </table>
                         </div>
